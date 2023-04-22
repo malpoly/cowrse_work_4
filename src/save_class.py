dbb02c9518ad9872863d4dcc56359e875c74dea0
malpoly <marry2691@yandex.ru>
2023-04-22 12:36:40 +0300

    Создан абстрактный класс, для добавления вакансий в файл json, получения данных из файла json по указанным критериям

---
+++ src/save_class.py
@@ -0,0 +1,8 @@
+from abc import ABC, abstractmethod
+
+
+class Save(ABC):
+    """Абстрактный класс, для добавления вакансий в файл json,
+    получения данных из файла json по указанным критериям"""
+    pass
+

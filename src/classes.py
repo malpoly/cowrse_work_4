236175dc3f3511a9363184c8e75bf91d491cfbe8
malpoly <marry2691@yandex.ru>
2023-04-26 12:04:22 +0300

    Доделан класс JSONSaver, внесены изменения в классы HH и SJ - параметр website

--- src/classes.py
+++ src/classes.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import os.path
+import re
 from abc import ABC, abstractmethod
 
 
@@ -25,7 +27,8 @@ class Vacancy():
     :param description - краткое описание
     """
 
-    def __init__(self, name, url, salary_from, salary_to, description)-> None:
+    def __init__(self, website, name, url, salary_from, salary_to, description)-> None:
+        self.website: str = website
         self.name: str = name
         self.url: str = url
         self.salary_from: int = salary_from
@@ -33,7 +36,8 @@ class Vacancy():
         self.description = description
 
     def __str__(self):
-        return f"name: {self.name},\n" \
+        return f"website: {self.website},\n" \
+               f"name: {self.name},\n" \
                f"url: {self.url},\n" \
                f"salary:{self.salary_from if self.salary_from else 0} -> " \
                f"{self.salary_to if self.salary_to else ''} руб/мес\n" \
@@ -61,6 +65,7 @@ class HHAPI(Engine):
             vacancies_data = data['items']
             vacancies = []
             for vacancy in vacancies_data:
+                website = "headhunter"
                 name = vacancy['name']
                 if vacancy['salary']['from'] is None:
                     salary_from = 0
@@ -78,7 +83,7 @@ class HHAPI(Engine):
                         salary_to = vacancy['salary']['to'] * EXCHANGE_RATE
                 description = vacancy['snippet']['requirement']
                 url = vacancy['alternate_url']
-                vacancy = Vacancy(name, url, salary_from, salary_to, description)
+                vacancy = Vacancy(website, name, url, salary_from, salary_to, description)
                 vacancies.append(vacancy)
             return vacancies
         else:
@@ -107,24 +112,25 @@ class SJAPI(Engine):
             vacancies_data = data['objects']
             vacancies = []
             for vacancy in vacancies_data:
+                website = "superjob"
                 name = vacancy['profession']
                 if vacancy['payment_from'] is None:
                     salary_from = 0
                 else:
-                    if vacancy['currency'] == 'RUR':
+                    if vacancy['currency'] == 'rub':
                         salary_from = vacancy['payment_from']
                     else:
                         salary_from = vacancy['payment_from'] * EXCHANGE_RATE
                 if vacancy['payment_to'] is None:
                     salary_to = 0
                 else:
-                    if vacancy['currency'] == 'RUR':
+                    if vacancy['currency'] == 'rub':
                         salary_to = vacancy['payment_to']
                     else:
                         salary_to = vacancy['payment_to'] * EXCHANGE_RATE
                 description = vacancy['candidat']
                 url = vacancy['link']
-                vacancy = Vacancy(name, url, salary_from, salary_to, description)
+                vacancy = Vacancy(website, name, url, salary_from, salary_to, description)
                 vacancies.append(vacancy)
             return vacancies
         else:
@@ -136,39 +142,46 @@ class Save(ABC):
     получения данных из файла json по указанным критериям"""
 
     @abstractmethod
-    def save_hh(self, headhunter):
-        """
-        Cохраняет данные о вакансиях в файл JSON
-        :param headhunter - список с вакансиями с HH
-        """
+    def save_json(self, data_hh: list[dict], data_sj: list[dict]) -> None:
         pass
 
 
 class JSONSaver(Save):
     """Класс для сбора данных о вакансиях с двух сайтов, их анализу и сохранению в файл"""
-    def __init__(self):
+    def __init__(self) -> None:
         """Инициализатор класс"""
-        self.file_name: str = 'Vacancy.json'
+        self.filename = "VacancyJson"  # при инициализации экземпляра
 
-    def save_hh(self, headhunter):
+    def save_json(self, data_hh: list[dict]=None, data_sj: list[dict]=None) -> None:
         """
         Cохраняет данные о вакансиях в файл JSON
-        :param headhunter - список с вакансиями с HH
+        :param data_hh - список с вакансиями с HH, по умолчанию None
+        :param data_sj - список с вакансиями с SJ, по умолчанию None
         """
-        with open(self.file_name, 'w', encoding='utf-8') as file:
-            json.dump(headhunter, file, ensure_ascii=False, indent=4)
+        if data_hh is not None and data_sj is None:
+            # вариант, когда нет данных по сайту SJ
+            with open(self.filename, 'w', encoding='utf-8') as file:
+                json.dump(sorted([vars(vacancy) for vacancy in data_hh], key=lambda x: x['salary_from'],
+                                 reverse=True), file, ensure_ascii=False, indent=4)
+        elif data_hh is None and data_sj is not None:
+            # вариант, когда нет данных по сайту HH
+            with open(self.filename, 'w', encoding='utf-8') as file:
+                json.dump(sorted([vars(vacancy) for vacancy in data_sj], key=lambda x: x['salary_from'],
+                                 reverse=True), file, ensure_ascii=False, indent=4)
+        elif data_hh is None and data_sj is None:
+            print('Нет данных по вакансиям')
+        else:
+            with open(self.filename, 'w', encoding='utf-8') as file:
+                json.dump(sorted([vars(vacancy) for vacancy in data_hh + data_sj], key=lambda x: x['salary_from'],
+                                 reverse=True),file, ensure_ascii=False, indent=4)
 
 
 test_sj = SJAPI()
-my_test = test_sj.get_vacancy("python")
-for i in my_test:
-    print(i)
-
-#test_hh = HHAPI()
-#my_test = test_hh.get_vacancy("python")
-#for i in my_test:
-    #print(i)
-
+my_test_sj = test_sj.get_vacancy("python")
+test_hh = HHAPI()
+my_test_hh = test_hh.get_vacancy("python")
+test_json_hh = JSONSaver()
+test_json_hh.save_json(my_test_hh, my_test_sj)
 
 
 
